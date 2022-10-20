52409918899ee2fdc076b51916d314973793a409
ibublXiuWei <ibub.l.232@gmail.com>
2022-10-20 13:00:19 +0800

    更新

--- Q4/Program.cs
+++ Q4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Q4
@@ -10,30 +11,21 @@ namespace Q4
 	{
 		static void Main(string[] args)
 		{
-			int hundred = 100;
-			int sum = 100;
-			int Cock=5;
-			int Hen=3;
-			int Chicken=1;
-			int a;
-			int b;
-			int c;
-			int c1 = 3 * c;//三隻1元
-			//int sum100 = a + b + c;
-			
-			for (int sum100 = 100; a < length; a++)
+			int count = new int();
+			for(int cock = 0; cock <= 100/5; cock++)
 			{
-				for (int b = 0; b < length; b++)
-				{
-					for (int c = 0; c < length; c++)
+				for(int hen = 0; hen <=(100-5*cock)/3 ; hen++)
 					{
-
+						count++;
+					int chicken = 100 - cock - hen;
+					double sum = 5*cock +3* hen + (double)chicken / 3.0;
+					if(sum==100)
+					{
+						Console.WriteLine($"共有{cock}隻公雞，{hen}隻母雞，{chicken}隻小雞");
 					}
-
-				}
+					}
 			}
-			
-
+			//Console.WriteLine(count);
 		}
 	}
 }
